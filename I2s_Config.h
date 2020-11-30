6d57083449d387c97c122ea51308055a3df57172
Enrique  Teran <31829881+enriqueter@users.noreply.github.com>
2020-11-29 19:59:28 -0600

    Initial Files

---
+++ I2s_Config.h
@@ -0,0 +1,20 @@
+/*
+ * I2s_Config.h
+ *
+ *  Created on: 26 nov. 2020
+ *      Author: etera
+ */
+
+#ifndef I2S_CONFIG_H_
+#define I2S_CONFIG_H_
+
+#include "fsl_sai.h"
+#include "fsl_i2c.h"
+#include "fsl_uart.h"
+#include "freertos_i2c.h"
+#include "wm8731.h"
+
+
+
+
+#endif /* I2S_CONFIG_H_ */
